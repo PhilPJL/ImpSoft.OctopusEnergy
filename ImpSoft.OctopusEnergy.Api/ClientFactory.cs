64317e94d3996c8bea3e10354f29d0e97b817058
Philip Lee <github@pjlconsultants.co.uk>
2020-06-30 17:58:54 +0100

    Make Http compression optional

--- ImpSoft.OctopusEnergy.Api/ClientFactory.cs
+++ ImpSoft.OctopusEnergy.Api/ClientFactory.cs
@@ -6,18 +6,27 @@
         ///     Creates a client with access to public and private data.
         /// </summary>
         /// <param name="apiKey">An API key allocated at tbd</param>
-        public static IPrivateClient Create(string apiKey)
+        /// <param name="enableAutomaticCompression">By default this will attempt to enable GZip and Deflate compression.  Some HttpClientHandler implementations (Blazor WASM) don't support the AutomaticCompression property. 
+        /// Set the property in those cases.  Set to false to disable compression where AutomaticCompression is supported.</param>
+        public static IPrivateClient Create(string apiKey, bool? enableAutomaticCompression = true)
         {
-            return new PrivateClient(apiKey);
+            return new PrivateClient(apiKey, enableAutomaticCompression);
         }
 
         /// <summary>
         ///     Creates a client with access to public data.
         /// </summary>
+        /// <param name="enableAutomaticCompression">By default this will attempt to enable GZip and Deflate compression.  Some HttpClientHandler implementations (Blazor WASM) don't support the AutomaticCompression property. 
+        /// Set the property to null in those cases.  Set to false to disable compression where AutomaticCompression is supported.</param>
         /// <returns></returns>
-        public static IPublicClient Create()
+        public static IPublicClient Create(bool? enableAutomaticCompression = true)
         {
-            return new PublicClient();
+            return new PublicClient(enableAutomaticCompression);
         }
+
+        /// <summary>
+        /// The base URL of the Octopus Energy API
+        /// </summary>
+        public static string BaseUrl { get; } = "https://api.octopus.energy";
     }
 }
