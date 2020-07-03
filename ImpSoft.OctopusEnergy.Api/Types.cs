94d2935a736b034dda825e302fc53f58ae2ae163
Philip Lee <github@pjlconsultants.co.uk>
2020-07-03 11:18:44 +0100

    Remove dependency on System.ServiceModel.Primitives

--- ImpSoft.OctopusEnergy.Api/Types.cs
+++ ImpSoft.OctopusEnergy.Api/Types.cs
@@ -1,24 +1,22 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 
 namespace ImpSoft.OctopusEnergy.Api
 {
-    [DataContract(Name = "")]
     public class PagedResults<TResult>
     {
-        [DataMember(Name = "count")] public int Count { get; set; }
-        [DataMember(Name = "next")] public string Next { get; set; }
-        [DataMember(Name = "previous")] public string Previous { get; set; }
-        [DataMember(Name = "results")] public IEnumerable<TResult> Results { get; set; }
+        [JsonProperty("count")] public int Count { get; set; }
+        [JsonProperty("next")] public string Next { get; set; }
+        [JsonProperty("previous")] public string Previous { get; set; }
+        [JsonProperty("results")] public IEnumerable<TResult> Results { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class Consumption
     {
-        [DataMember(Name = "consumption")] public decimal Quantity { get; set; }
-        [DataMember(Name = "interval_start")] public DateTimeOffset Start { get; set; }
-        [DataMember(Name = "interval_end")] public DateTimeOffset End { get; set; }
+        [JsonProperty("consumption")] public decimal Quantity { get; set; }
+        [JsonProperty("interval_start")] public DateTimeOffset Start { get; set; }
+        [JsonProperty("interval_end")] public DateTimeOffset End { get; set; }
     }
 
 #pragma warning disable CA1027 // Mark enums with FlagsAttribute
@@ -41,199 +39,187 @@ namespace ImpSoft.OctopusEnergy.Api
         Night
     }
 
-    [DataContract(Name = "")]
     public class ProductBase
     {
-        [DataMember(Name = "code")] public string Code { get; set; }
-        [DataMember(Name = "full_name")] public string FullName { get; set; }
-        [DataMember(Name = "display_name")] public string DisplayName { get; set; }
-        [DataMember(Name = "description")] public string Description { get; set; }
-        [DataMember(Name = "is_variable")] public bool IsVariable { get; set; }
-        [DataMember(Name = "is_green")] public bool IsGreen { get; set; }
-        [DataMember(Name = "is_tracker")] public bool IsTracker { get; set; }
-        [DataMember(Name = "is_prepay")] public bool IsPrepay { get; set; }
-        [DataMember(Name = "is_business")] public bool IsBusiness { get; set; }
-        [DataMember(Name = "is_restricted")] public bool IsRestricted { get; set; }
-        [DataMember(Name = "term")] public int? Term { get; set; }
-        [DataMember(Name = "brand")] public string Brand { get; set; }
-        [DataMember(Name = "available_from")] public DateTimeOffset AvailableFrom { get; set; }
-        [DataMember(Name = "available_to")] public DateTimeOffset? AvailableTo { get; set; }
+        [JsonProperty("code")] public string Code { get; set; }
+        [JsonProperty("full_name")] public string FullName { get; set; }
+        [JsonProperty("display_name")] public string DisplayName { get; set; }
+        [JsonProperty("description")] public string Description { get; set; }
+        [JsonProperty("is_variable")] public bool IsVariable { get; set; }
+        [JsonProperty("is_green")] public bool IsGreen { get; set; }
+        [JsonProperty("is_tracker")] public bool IsTracker { get; set; }
+        [JsonProperty("is_prepay")] public bool IsPrepay { get; set; }
+        [JsonProperty("is_business")] public bool IsBusiness { get; set; }
+        [JsonProperty("is_restricted")] public bool IsRestricted { get; set; }
+        [JsonProperty("term")] public int? Term { get; set; }
+        [JsonProperty("brand")] public string Brand { get; set; }
+        [JsonProperty("available_from")] public DateTimeOffset AvailableFrom { get; set; }
+        [JsonProperty("available_to")] public DateTimeOffset? AvailableTo { get; set; }
 
-        [DataMember(Name = "links")] public IEnumerable<Link> Links { get; set; }
+        [JsonProperty("links")] public IEnumerable<Link> Links { get; set; }
     }
 
     public class Product : ProductBase
     {
-        [DataMember(Name = "direction")] public string Direction { get; set; }
+        [JsonProperty("direction")] public string Direction { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class ProductDetail : ProductBase
     {
-        [DataMember(Name = "tariffs_active_at")]
+        [JsonProperty("tariffs_active_at")]
         public DateTimeOffset ActiveAt { get; set; }
 
-        [DataMember(Name = "single_register_electricity_tariffs")]
+        [JsonProperty("single_register_electricity_tariffs")]
         public Dictionary<string, TariffsByPeriod> SingleRegisterElectricityTariffs { get; set; }
 
-        [DataMember(Name = "dual_register_electricity_tariffs")]
+        [JsonProperty("dual_register_electricity_tariffs")]
         public Dictionary<string, TariffsByPeriod> DualRegisterElectricityTariffs { get; set; }
 
-        [DataMember(Name = "single_register_gas_tariffs")]
+        [JsonProperty("single_register_gas_tariffs")]
         public Dictionary<string, TariffsByPeriod> SingleRegisterGasTariffs { get; set; }
 
-        [DataMember(Name = "sample_quotes")] public Dictionary<string, SampleQuotesByPeriod> SampleQuotes { get; set; }
+        [JsonProperty("sample_quotes")] public Dictionary<string, SampleQuotesByPeriod> SampleQuotes { get; set; }
 
-        [DataMember(Name = "sample_consumption")]
+        [JsonProperty("sample_consumption")]
         public SampleConsumptionByRate SampleConsumption { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class SampleConsumptionByRate
     {
-        [DataMember(Name = "electricity_single_rate")]
+        [JsonProperty("electricity_single_rate")]
         public SampleConsumption ElectricitySingleRate { get; set; }
 
-        [DataMember(Name = "electricity_dual_rate")]
+        [JsonProperty("electricity_dual_rate")]
         public SampleConsumption ElectricityDualRate { get; set; }
 
-        [DataMember(Name = "dual_fuel_single_rate")]
+        [JsonProperty("dual_fuel_single_rate")]
         public SampleConsumption DualFuelSingleRate { get; set; }
 
-        [DataMember(Name = "dual_fuel_dual_rate")]
+        [JsonProperty("dual_fuel_dual_rate")]
         public SampleConsumption DualFuelDualRate { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class SampleConsumption
     {
-        [DataMember(Name = "electricity_standard")]
+        [JsonProperty("electricity_standard")]
         public decimal? ElectricityStandard { get; set; }
 
-        [DataMember(Name = "electricity_day")] public decimal? ElectricityDay { get; set; }
+        [JsonProperty("electricity_day")] public decimal? ElectricityDay { get; set; }
 
-        [DataMember(Name = "electricity_night")]
+        [JsonProperty("electricity_night")]
         public decimal? ElectricityNight { get; set; }
 
-        [DataMember(Name = "gas_standard")] public decimal? GasStandard { get; set; }
+        [JsonProperty("gas_standard")] public decimal? GasStandard { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class TariffsByPeriod
     {
-        [DataMember(Name = "direct_debit_monthly")]
+        [JsonProperty("direct_debit_monthly")]
         public Tariff Monthly { get; set; }
 
-        [DataMember(Name = "direct_debit_quarterly")]
+        [JsonProperty("direct_debit_quarterly")]
         public Tariff Quarterly { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class Tariff
     {
-        [DataMember(Name = "code")] public string Code { get; set; }
+        [JsonProperty("code")] public string Code { get; set; }
 
-        [DataMember(Name = "standing_charge_exc_vat")]
+        [JsonProperty("standing_charge_exc_vat")]
         public decimal StandingChargeExcludingVAT { get; set; }
 
-        [DataMember(Name = "standing_charge_inc_vat")]
+        [JsonProperty("standing_charge_inc_vat")]
         public decimal StandingChargeIncludingVAT { get; set; }
 
-        [DataMember(Name = "online_discount_exc_vat")]
+        [JsonProperty("online_discount_exc_vat")]
         public decimal OnlineDiscountExcludingVAT { get; set; }
 
-        [DataMember(Name = "online_discount_inc_vat")]
+        [JsonProperty("online_discount_inc_vat")]
         public decimal OnlineDiscountIncludingVAT { get; set; }
 
-        [DataMember(Name = "dual_fuel_discount_exc_vat")]
+        [JsonProperty("dual_fuel_discount_exc_vat")]
         public decimal DualFuelDiscountExcludingVAT { get; set; }
 
-        [DataMember(Name = "dual_fuel_discount_inc_vat")]
+        [JsonProperty("dual_fuel_discount_inc_vat")]
         public decimal DualFuelDiscountIncludingVAT { get; set; }
 
-        [DataMember(Name = "exit_fees_exc_vat")]
+        [JsonProperty("exit_fees_exc_vat")]
         public decimal ExitFeesExcludingVAT { get; set; }
 
-        [DataMember(Name = "exit_fees_inc_vat")]
+        [JsonProperty("exit_fees_inc_vat")]
         public decimal ExitFeesIncludingVAT { get; set; }
 
-        [DataMember(Name = "standard_unit_rate_exc_vat")]
+        [JsonProperty("standard_unit_rate_exc_vat")]
         public decimal? StandardUnitRateExcludingVAT { get; set; }
 
-        [DataMember(Name = "standard_unit_rate_inc_vat")]
+        [JsonProperty("standard_unit_rate_inc_vat")]
         public decimal? StandardUnitRateIncludingVAT { get; set; }
 
-        [DataMember(Name = "day_unit_rate_exc_vat")]
+        [JsonProperty("day_unit_rate_exc_vat")]
         public decimal? DayUnitRateExcludingVAT { get; set; }
 
-        [DataMember(Name = "day_unit_rate_inc_vat")]
+        [JsonProperty("day_unit_rate_inc_vat")]
         public decimal? DayUnitRateIncludingVAT { get; set; }
 
-        [DataMember(Name = "night_unit_rate_exc_vat")]
+        [JsonProperty("night_unit_rate_exc_vat")]
         public decimal? NightUnitRateExcludingVAT { get; set; }
 
-        [DataMember(Name = "night_unit_rate_inc_vat")]
+        [JsonProperty("night_unit_rate_inc_vat")]
         public decimal? NightUnitRateIncludingVAT { get; set; }
 
-        [DataMember(Name = "links")] public IEnumerable<Link> Links { get; set; }
+        [JsonProperty("links")] public IEnumerable<Link> Links { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class SampleQuotesByPeriod
     {
-        [DataMember(Name = "direct_debit_monthly")]
+        [JsonProperty("direct_debit_monthly")]
         public SampleQuotes Monthly { get; set; }
 
-        [DataMember(Name = "direct_debit_quarterly")]
+        [JsonProperty("direct_debit_quarterly")]
         public SampleQuotes Quarterly { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class SampleQuotes
     {
-        [DataMember(Name = "electricity_single_rate")]
+        [JsonProperty("electricity_single_rate")]
         public SampleQuote ElectricitySingleRate { get; set; }
 
-        [DataMember(Name = "electricity_dual_rate")]
+        [JsonProperty("electricity_dual_rate")]
         public SampleQuote ElectricityDualRate { get; set; }
 
-        [DataMember(Name = "dual_fuel_single_rate")]
+        [JsonProperty("dual_fuel_single_rate")]
         public SampleQuote DualFuelSingleRate { get; set; }
 
-        [DataMember(Name = "dual_fuel_dual_rate")]
+        [JsonProperty("dual_fuel_dual_rate")]
         public SampleQuote DualFuelDualRate { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class SampleQuote
     {
-        [DataMember(Name = "annual_cost_inc_vat")]
+        [JsonProperty("annual_cost_inc_vat")]
         public decimal? AnnualCostIncludingVAT { get; set; }
 
-        [DataMember(Name = "annual_cost_exc_vat")]
+        [JsonProperty("annual_cost_exc_vat")]
         public decimal? AnnualCostExcludingVAT { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class Link
     {
-        [DataMember(Name = "href")] public string HRef { get; set; }
-        [DataMember(Name = "method")] public string Method { get; set; }
-        [DataMember(Name = "rel")] public string Rel { get; set; }
+        [JsonProperty("href")] public string HRef { get; set; }
+        [JsonProperty("method")] public string Method { get; set; }
+        [JsonProperty("rel")] public string Rel { get; set; }
     }
 
-    [DataContract(Name = "")]
     internal class MeterPointGridSupplyPoint
     {
-        [DataMember(Name = "gsp")] public string GroupId { get; set; }
-        [DataMember(Name = "mpan")] public string MPan { get; set; }
-        [DataMember(Name = "profile_class")] public int ProfileClass { get; set; }
+        [JsonProperty("gsp")] public string GroupId { get; set; }
+        [JsonProperty("mpan")] public string MPan { get; set; }
+        [JsonProperty("profile_class")] public int ProfileClass { get; set; }
     }
 
-    [DataContract(Name = "")]
     internal class GridSupplyPoint
     {
-        [DataMember(Name = "group_id")] public string GroupId { get; set; }
+        [JsonProperty("group_id")] public string GroupId { get; set; }
     }
 
     public class GridSupplyPointInfo
@@ -243,13 +229,12 @@ namespace ImpSoft.OctopusEnergy.Api
         public string Area { get; set; }
     }
 
-    [DataContract(Name = "")]
     public class Charge
     {
-        [DataMember(Name = "value_exc_vat")] public decimal ValueExcludingVAT { get; set; }
-        [DataMember(Name = "value_inc_vat")] public decimal ValueIncludingVAT { get; set; }
-        [DataMember(Name = "valid_from")] public DateTimeOffset? ValidFrom { get; set; }
-        [DataMember(Name = "valid_to")] public DateTimeOffset? ValidTo { get; set; }
+        [JsonProperty("value_exc_vat")] public decimal ValueExcludingVAT { get; set; }
+        [JsonProperty("value_inc_vat")] public decimal ValueIncludingVAT { get; set; }
+        [JsonProperty("valid_from")] public DateTimeOffset? ValidFrom { get; set; }
+        [JsonProperty("valid_to")] public DateTimeOffset? ValidTo { get; set; }
 
         public DateTimeOffset ValidFromUTC => ValidFrom?.ToUniversalTime() ?? DateTimeOffset.MinValue;
         public DateTimeOffset ValidToUTC => ValidTo?.ToUniversalTime() ?? DateTimeOffset.MaxValue;
